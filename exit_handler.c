66c93c466b9bdf5ab26dbbf33e7bd3c94f0bb50f
SiMoG97 <echaaranimohamed@gmail.com>
2023-05-24 09:25:48 -0700

    try to fix exit_handler function again

--- exit_handler.c
+++ exit_handler.c
@@ -11,11 +11,34 @@
 
 void exit_handler(char **args, size_t nbr_of_args)
 {
-	int status = EXIT_SUCCESS;
+	int status = EXIT_SUCCESS, i;
 
-	if (args[1])
-		status = _atoi(args[1]);
+	if (nbr_of_args > 1)
+	{
+		for (i = 0; args[1][i] != '\0'; i++)
+		{
+			if (!isdigit(args[1][i]))
+			{
+				perror("Illegal number");
+				free_vector(args, nbr_of_args);
+				return;
+			}
+		}
+		status = atoi(args[1]);
+	}
 
 	free_vector(args, nbr_of_args);
 	exit(status);
 }
+/*
+*void exit_handler(char **args, size_t nbr_of_args)
+*{
+*	int status = EXIT_SUCCESS;
+*
+*	if (args[1])
+*		status = _atoi(args[1]);
+*
+*	free_vector(args, nbr_of_args);
+*	exit(status);
+*}
+*/
